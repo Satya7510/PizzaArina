5c55b6d28ddea32a87b454cc3799d2f7fb9852d6
Satya Prakash Pandey <satyaprakashson2017@gmail.com>
2022-08-24 22:14:40 +0530

    add cart page,auth

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-// app.get('/', (req, res) => {
-//     res.render('home')
-// })
 
 // const mongoose = require('mongoose')
 // const session = require('express-session')
@@ -36,6 +30,19 @@ app.use(express.static('public'))
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 // require('./routes/web')(app)
 // app.use((req, res) => {
