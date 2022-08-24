9fdef2d9f4de6ebe2f3390a23ed487e0a309cdb8
Satya Prakash Pandey <satyaprakashson2017@gmail.com>
2022-08-24 10:56:43 +0530

    make home page

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+// mix.js('src/app.js', 'dist').setPublicPath('dist');
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css');
+
+
+
+mix.babelConfig({
+    "plugins": ["@babel/plugin-proposal-class-properties"]
+});
