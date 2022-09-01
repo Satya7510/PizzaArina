2dd472b18012be414b085db630083e052ab9798b
Satya Prakash Pandey <satyaprakashson2017@gmail.com>
2022-09-02 00:58:00 +0530

    login and logout problem fixed

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDBStore = require('connect-mongo')
+const passport = require('passport')
 
 
 // Database connection
@@ -24,6 +25,11 @@ const connectDB = async () => {
 }
 
 connectDB();
+
+
+
+
+
 
 
 //Session store
@@ -43,17 +49,24 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 }   //24 hours
 }))
 
+//Passport Config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 // Assets
 app.use(express.static('public'))
-app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
 // app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
-    // res.locals.user = req.user
+    res.locals.user = req.user
     next()
 })
 
